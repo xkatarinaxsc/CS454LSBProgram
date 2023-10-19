8ef9173e62050df04cf41b602ab89ad7b01c2fdb
katandflowers <katarina.ag.scott@gmail.com>
2023-10-19 18:13:42 +0100

    creating pixel array

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy as np
 
 
 # Convert image to 24-bit BMP
@@ -32,6 +33,10 @@ def menu_option_2():
 
 
 def hide_secret_message(bin_msg, bmp_img, encrypted_img_name):
+    pixel_array = np.array(list(bmp_img.getdata()))
+    total_pixels = pixel_array.size//3
+
+
 
 
 
@@ -46,6 +51,9 @@ def convert_string_to_binary(msg_text):
     for char in msg_text:
         binary_char = format(ord(char), '08b')
         binary_text += binary_char
+
+    # Adding a delimiter
+    binary_text += "$Hq73Op"
 
     return binary_text
 
@@ -63,15 +71,15 @@ def convert_binary_to_string(binary_msg):
 
 # Check the string will fit into the image
 def size_check(bmp_img, bin_msg):
-     width, height = bmp_img.size
-     total_pixels = width * height
+    width, height = bmp_img.size
+    total_pixels = width * height
 
-     req_pixels = len(bin_msg)
+    req_pixels = len(bin_msg)
 
-     if req_pixels < total_pixels:
-         return 1
-     else:
-         return 0
+    if req_pixels < total_pixels:
+        return 1
+    else:
+        return 0
 
 
 
